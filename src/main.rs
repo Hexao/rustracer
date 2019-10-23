8250184e971613dd9f6eb1a49c218a740d70691e
Alexandre ANDRE <les.jeux777@gmail.com>
2019-10-23 19:36:09 +0200

    class Vector - add by ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+pub mod vector;
+
 fn main() {
+    let vec1 = vector::Vector::default();
+    let vec2 = vector::Vector::new(1.0, 1.0, 1.0);
+
+    let vec3 = &vec1 + &vec2;
+
     println!("Hello, world!");
+    println!("{:?}", vec3);
 }
